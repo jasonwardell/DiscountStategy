3cafe64ba5ed6c1c16d91642bd1f6bdc4330493a
klwortman <klwortman@192.168.1.5>
2015-09-30 22:15:01 -0500

    not working LineItem Class

--- src/discountstrategy/LineItem.java
+++ src/discountstrategy/LineItem.java
@@ -8,13 +8,21 @@ public class LineItem {
 
     private DiscountStategy discount;
     private Product prodId;
+    private double qty;
     private Product name;
     private Product unitPrice;
     private double total;
 
+    public double getQty() {
+        return qty;
+    }
+
+    public void setQty(double qty) {
+        this.qty = qty;
+    }
 
     public double getTotal(double unitPrice, double qty) {
-        total = unitPrice - discount.getDiscountAmt(unitPrice, qty);
+        total = (unitPrice * qty) - discount.getDiscountAmt(unitPrice, qty);
         return total;
     }
 
@@ -55,11 +63,12 @@ public class LineItem {
     }
 
     public static void main(String[] args) {
-        LineItem li = new LineItem();
+
         Product p2 = new Product("A100", 20.00, "BaseBall Hat",
                 new PercentOffDiscount(.10));
+              LineItem li = new LineItem();
         double amt = p2.getDiscountAmt(1);
-        double total = li.getTotal(20.00, 1.0);
+        double total = li.getTotal(20.00, 1);
         System.out.println("ProdID         unitPrice          Product        Discount    Total");
         System.out.println(p2.getProdId() + "           " + p2.getUnitPrice() + "                "
                 + p2.getName() + "    " + amt + "         " + total);
