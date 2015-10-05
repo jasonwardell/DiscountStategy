7d7a0e6e66bde55f65fc6809886e67559c985c52
klwortman <klwortman@192.168.1.5>
2015-10-04 19:46:39 -0500

    updated Receipt class

--- src/discountstrategy/Receipt.java
+++ src/discountstrategy/Receipt.java
@@ -5,5 +5,43 @@ package discountstrategy;
  * @author jwardell
  */
 public class Receipt {
-    
+
+    private double qty;
+    private Product product;
+    private double totalPrice;
+    private String receipt;
+
+    public double getQty() {
+        return qty;
+    }
+
+    public void setQty(double qty) {
+        this.qty = qty;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public void setReceipt(String receipt) {
+        this.receipt = receipt;
+    }
+
+    public String getReceipt() {
+        receipt = (product.getProdId() + " " + product.getName() + " " + product.getUnitPrice() + " "
+                + qty + " " + product.getDiscountAmt(qty) + " " + totalPrice);
+        return receipt;
+    }
 }
